e5f4823b1678830150813248e261df26f97d45c9
slyk26 <slyk26@proton.me>
2023-06-23 21:18:23 +0200

    writing markov messages

--- src/handler/events/message.rs
+++ src/handler/events/message.rs
@@ -1,10 +1,39 @@
-use serenity::model::prelude::Message;
+use std::sync::Arc;
+use rand::{Rng, thread_rng};
+use serenity::http::Http;
+use serenity::model::prelude::{ChannelId, Message};
 use serenity::prelude::Context;
 use sqlx::{Pool, Postgres};
 use crate::models::MarkovModel;
 use crate::service::markov_service::MarkovService;
 
-pub async fn call(_ctx: &Context, new_message: Message, db: &Pool<Postgres>) {
+pub async fn call(ctx: &Context, new_message: Message, db: &Pool<Postgres>) {
+    destruct_message(&new_message, db).await;
+
+    if !new_message.author.bot &&
+        (thread_rng().gen_range(0..100) < 7 || mentions_me_wrapper(ctx.http.clone(), &new_message).await) {
+        send_message(ctx, new_message.channel_id, db).await;
+    }
+}
+
+async fn mentions_me_wrapper(cache: Arc<Http>, msg: &Message) -> bool {
+    match msg.mentions_me(cache).await {
+        Ok(b) => b,
+        Err(e) => {
+            error!("{}", e);
+            false
+        }
+    }
+}
+
+async fn send_message(ctx: &Context, channel: ChannelId, db: &Pool<Postgres>) {
+    let message = MarkovService::generate_message(db).await;
+    let _ = channel.send_message(&ctx.http, |m| {
+        m.content(message)
+    }).await;
+}
+
+async fn destruct_message(new_message: &Message, db: &Pool<Postgres>) {
     if !new_message.author.bot {
         let words = new_message.content.split(' ').collect::<Vec<&str>>();
 
@@ -14,7 +43,7 @@ pub async fn call(_ctx: &Context, new_message: Message, db: &Pool<Postgres>) {
             if i + 1 < words.len() {
                 next = Some(words[i + 1].to_string());
             }
-            MarkovService::process(db, MarkovModel{
+            MarkovService::store(db, MarkovModel {
                 id: 0,
                 guild_id: new_message.guild_id.unwrap().to_string(),
                 current_word: current,
--- src/service/markov_service.rs
+++ src/service/markov_service.rs
@@ -1,13 +1,14 @@
-use sqlx::{Postgres, Pool, Error, query, Row};
+use sqlx::{Postgres, Pool, Error, query, Row, query_as};
+use rand::{Rng, thread_rng};
 use crate::models::MarkovModel;
 
 pub struct MarkovService;
 
 impl MarkovService {
-    pub async fn process(db: &Pool<Postgres>, part: MarkovModel) {
+    pub async fn store(db: &Pool<Postgres>, part: MarkovModel) {
         if let Some(id) = MarkovService::check_if_exists(db, &part).await {
             if let Some(freq) = MarkovService::get_frequency(db, id).await {
-                let _ = MarkovService::update(db, id, freq+1).await;
+                let _ = MarkovService::update(db, id, freq + 1).await;
             }
         } else {
             let _ = MarkovService::insert(db, &part).await;
@@ -75,6 +76,69 @@ impl MarkovService {
                 warn!("get_frequency: {}", e);
                 None
             }
+        }
+    }
+
+    async fn get_max(db: &Pool<Postgres>) -> i32 {
+        match query("select max(id) from markov_data")
+            .fetch_one(db).await {
+            Ok(row) => row.get::<i32, _>(0),
+            Err(e) => {
+                warn!("get_max: {}", e);
+                0
+            }
+        }
+    }
+
+    async fn get_by_id(db: &Pool<Postgres>, id: i32) -> Result<MarkovModel, Error> {
+        query_as("select * from markov_data where id = $1")
+            .bind(id)
+            .fetch_one(db).await
+    }
+
+    pub async fn generate_message(db: &Pool<Postgres>) -> String {
+        let max = MarkovService::get_max(db).await;
+        debug!("generating message with {} entries", max);
+        let mut msg = String::new();
+
+        if max < 1000 {
+            warn!("there is not enough data to work with");
+            return msg;
+        }
+
+        let start_id = thread_rng().gen_range(1..=max);
+
+        if let Ok(start) = MarkovService::get_by_id(db, start_id).await {
+            let mut part = start;
+
+            while msg.len() < 2000 {
+                msg.push_str(format!("{} ", part.current_word).as_str());
+
+                if let Some(next) = part.next_word {
+                    part = MarkovService::get_next(db, next, part.guild_id).await;
+                } else {
+                    debug!("return message: {}", msg);
+                    return msg;
+                }
+            }
+        } else {
+            // cannot get part with id
+            warn!("cannot start message generation")
+        }
+        msg
+    }
+
+    async fn get_next(db: &Pool<Postgres>, current_word: String, from_guild: String) -> MarkovModel {
+        if let Ok(possibilities) =
+            query_as::<_, MarkovModel>("select * from markov_data where current_word = $1 and guild_id = $2")
+                .bind(current_word)
+                .bind(from_guild)
+                .fetch_all(db).await {
+            let idx = thread_rng().gen_range(0..possibilities.len());
+            return possibilities[idx].clone()
+        } else {
+            error!("cannot fetch next markov part");
+            MarkovModel::empty()
         }
     }
 }
